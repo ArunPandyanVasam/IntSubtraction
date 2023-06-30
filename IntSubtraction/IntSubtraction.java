c5386e9c12864fef05363fbd3a4f4ce2930e1b6e
ArunPandyanVasam <you@example.com>
2023-06-30 16:25:32 -0400

    version1

---
+++ IntSubtraction/IntSubtraction.java
@@ -0,0 +1,10 @@
+package IntSubtraction;
+
+public class IntSubtraction {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 1;
+
+        System.out.println("x-y: " + (x - y));
+    }
+}
